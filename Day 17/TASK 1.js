addf1aa3392a003bef9e576ced11e8448091ef74
jeevajack30032000-cell <jeevajack30032000@gmail.com>
2025-12-02 19:42:08 +0530

    d17

--- Day 17/TASK 1.js
+++ Day 17/TASK 1.js
@@ -1,3 +1,3 @@
 // task 1: Print your name in the console using console.log().
 
-console.log("i'm jeevarathinam s from chennai.");
+console.log("i'm jeevarathinam S from chennai.");
---
+++ Day 17/task 3.js
@@ -0,0 +1,4 @@
+// task 3: Create a variable storing your age.
+
+let age=25;
+console.log(age);
