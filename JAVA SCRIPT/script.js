709b5814db4b7d4180a0cd4f4b7fb07b5662403b
jeevajack30032000-cell <jeevajack30032000@gmail.com>
2025-12-07 11:08:03 +0530

    .

--- JAVA SCRIPT/script.js
+++ JAVA SCRIPT/script.js
@@ -1,2 +1,18 @@
 let a=10;
 console.log(a);
+
+
+
+// function definition :
+//                       function is a block of code designed to perform to a particular task 
+//                       a function is executed when something invokes it (calls it)
+//                       a function runs when it is called it
+//                       it received input parameters and  a single output    (return value)
+
+
+//                       key words : function , return , call , invoke , parameters , arguments
+
+// function declaration
+function greet(name) {
+    return "Hello, " + name + "!";
+}
