8583213fddfdc057c71a1dc3dd5c23935f5783cf
jeevajack30032000-cell <jeevajack30032000@gmail.com>
2025-12-15 09:28:54 +0530

    1

---
+++ Day 21/Task 1 .js
@@ -0,0 +1,24 @@
+// Print numbers 1–10 using a for loop.
+
+// Print even numbers using a while loop.
+
+// Use a do…while to print 1 run even if condition is false.
+
+// Loop through an array using for…of.
+
+// Loop through an object using for…in.
+
+// task 1: Print numbers 1–10 using a for loop.
+
+
+// task 2: Print even numbers using a while loop.
+
+let b = 20;
+let i = 2;
+while(i <= b) {
+    console.log(i);
+    i += 2;
+}
+
+
+
